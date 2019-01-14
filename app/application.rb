37139b326bc4e7e16d776b8b17b05fc76dc85092
Nick Inman <nick@theoatmeal.com>
2019-01-14 18:24:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,11 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    binding.pry
     if #Morning
       resp.write "Morning"
     else #Afternoon
       resp.write "Afternoon"
-    end 
+    end
     resp.finish
   end
 
