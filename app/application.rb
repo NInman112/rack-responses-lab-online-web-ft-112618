163e7f6ca8fa9703cf2546d797758d6d952f9bce
Nick Inman <nick@theoatmeal.com>
2019-01-14 18:25:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    if Time < 2015,11,27,12,00#Morning
+    if Time < (12,00) #Morning
       resp.write "Morning"
     else #Afternoon
       resp.write "Afternoon"
